0bd92e291446be636a79b5f330d978c0d912ac35
Erik Stagg <estagg@spi.local>
2018-08-03 12:27:45 -0400

    gulp.config.js - slashes removed from local variables at the top of config and added to the lines below

--- gulp.config.js
+++ gulp.config.js
@@ -13,11 +13,17 @@ module.exports = function() {
      * destFolder: Production directory. Change this value to change the name of the production directory
      * where compiled files are put and where browsersync is run.
      * 
+     * NOTE: by default, folders starting with an underscore (_) are ignored in the source, except for partials
+     * This is being used by default as a way to separate html partials and html that is to be compiled, but
+     * can also be used to hide other source files. To turn this feature off, remove lines that start with:
+     *      '!' + sourceFolder
+     * 
+     * 
      */
     var projectName = 'gulp';
-    var sourceFolder = 'src/';
-    var htmlPartialsFolder = '_partials/';
-    var destFolder = 'app/';
+    var sourceFolder = 'src';
+    var htmlPartialsFolder = '_partials';
+    var destFolder = 'app';
 
     var config = {
 
@@ -31,68 +37,68 @@ module.exports = function() {
          */
 
         src: {
-            root: sourceFolder,
+            root: sourceFolder + '/',
             html: [
-                sourceFolder + '**/*.html',
-                sourceFolder + '**/*.htm',
-                '!' + sourceFolder + '**/_*/**/*', // exclude files/subfolders in folders starting with '_'
+                sourceFolder + '/**/*.html',
+                sourceFolder + '/**/*.htm',
+                '!' + sourceFolder + '/**/_*/**/*', // exclude files/subfolders in folders starting with '_'
             ],
 
             htmlPartials: [
-                sourceFolder + htmlPartialsFolder + '*.html',
-                sourceFolder + htmlPartialsFolder + '*.htm',
+                sourceFolder + '/' + htmlPartialsFolder + '/' + '*.html',
+                sourceFolder + '/' + htmlPartialsFolder + '/' + '*.htm',
             ],
 
             // all js for "gulp lint-js"
             alljs: [
-                sourceFolder + '**/*.js', //js files in src
+                sourceFolder + '/**/*.js', //js files in src
                 './*.js', //js files in root folder (not project files. Used to debug gulpfile.js and gulp.config.js)
-                '!' + sourceFolder + '**/_*/**/*', // exclude files/subfolders in folders starting with '_'
+                '!' + sourceFolder + '/**/_*/**/*', // exclude files/subfolders in folders starting with '_'
             ],
 
             // list source JS to be compiled in order into one file
             js: [
-                sourceFolder + 'js/jquery.js', //example of a common vendor file. Will be skipped if not found
-                sourceFolder + 'js/' + projectName + '.js', // our main project file if one exists
-                sourceFolder + 'js/**/*.js', // any .js left in /src/js/
-                '!' + sourceFolder + '**/_*/**/*', // exclude files/subfolders in folders starting with '_'
+                sourceFolder + '/js/jquery.js', //example of a common vendor file. Will be skipped if not found
+                sourceFolder + '/js/' + projectName + '.js', // our main project file if one exists
+                sourceFolder + '/js/**/*.js', // any .js left in /src/js/
+                '!' + sourceFolder + '/**/_*/**/*', // exclude files/subfolders in folders starting with '_'
             ],
             // list source CSS to be compiled in order into projectName.css
             css: [
-                sourceFolder + 'css/bootstrap.css', //example of a common vendor file. Will be skipped if not found
-                sourceFolder + 'css/' + projectName + '.css', // our main project file if one exists
-                sourceFolder + 'css/**/*.css', // any .css left in /src/css/
-                '!' + sourceFolder + '**/_*/**/*', // exclude files/subfolders in folders starting with '_'
+                sourceFolder + '/css/bootstrap.css', //example of a common vendor file. Will be skipped if not found
+                sourceFolder + '/css/' + projectName + '.css', // our main project file if one exists
+                sourceFolder + '/css/**/*.css', // any .css left in /src/css/
+                '!' + sourceFolder + '/**/_*/**/*', // exclude files/subfolders in folders starting with '_'
             ],
             // list source LESS to be compiled in order into projectName.css
             less: [
-                sourceFolder + 'less/' + projectName + '.less', // our main project file if one exists
-                sourceFolder + 'less/**/*.less', // any .less in /src/less/
-                '!' + sourceFolder + '**/_*/**/*', // exclude files/subfolders in folders starting with '_'
+                sourceFolder + '/less/' + projectName + '.less', // our main project file if one exists
+                sourceFolder + '/less/**/*.less', // any .less in /src/less/
+                '!' + sourceFolder + '/**/_*/**/*', // exclude files/subfolders in folders starting with '_'
             ],
             // list source SASS to be compiled in order into projectName.css
             sass: [
-                sourceFolder + 'sass/' + projectName + '.sass', // our main project file if one exists
-                sourceFolder + 'sass/' + projectName + '.scss', // our main project file if one exists
-                sourceFolder + 'scss/' + projectName + '.sass', // our main project file if one exists
-                sourceFolder + 'scss/' + projectName + '.scss', // our main project file if one exists
-                sourceFolder + 'sass/**/*.sass', // any .sass in /src/sass/
-                sourceFolder + 'sass/**/*.scss', // any .scss in /src/sass/
-                sourceFolder + 'scss/**/*.sass', // any .sass in /src/scss/
-                sourceFolder + 'scss/**/*.scss', // any .scss in /src/scss/
-                '!' + sourceFolder + '**/_*/**/*', // exclude files/subfolders in folders starting with '_'
+                sourceFolder + '/sass/' + projectName + '.sass', // our main project file if one exists
+                sourceFolder + '/sass/' + projectName + '.scss', // our main project file if one exists
+                sourceFolder + '/scss/' + projectName + '.sass', // our main project file if one exists
+                sourceFolder + '/scss/' + projectName + '.scss', // our main project file if one exists
+                sourceFolder + '/sass/**/*.sass', // any .sass in /src/sass/
+                sourceFolder + '/sass/**/*.scss', // any .scss in /src/sass/
+                sourceFolder + '/scss/**/*.sass', // any .sass in /src/scss/
+                sourceFolder + '/scss/**/*.scss', // any .scss in /src/scss/
+                '!' + sourceFolder + '/**/_*/**/*', // exclude files/subfolders in folders starting with '_'
             ],
             fonts: [
-                sourceFolder + 'fonts/**/*.*', // any files in /src/fonts,
-                '!' + sourceFolder + '**/_*/**/*', // exclude files/subfolders in folders starting with '_'
+                sourceFolder + '/fonts/**/*.*', // any files in /src/fonts,
+                '!' + sourceFolder + '/**/_*/**/*', // exclude files/subfolders in folders starting with '_'
             ],
             img: [
-                sourceFolder + 'images/**/*.*', // any files in /src/images
-                '!' + sourceFolder + '**/_*/**/*', // exclude files/subfolders in folders starting with '_'
+                sourceFolder + '/images/**/*.*', // any files in /src/images
+                '!' + sourceFolder + '/**/_*/**/*', // exclude files/subfolders in folders starting with '_'
             ],
             docs: [
-                sourceFolder + 'docs/**/*.*', // any files in /src/docs
-                '!' + sourceFolder + '**/_*/**/*', // exclude files/subfolders in folders starting with '_'
+                sourceFolder + '/docs/**/*.*', // any files in /src/docs
+                '!' + sourceFolder + '/**/_*/**/*', // exclude files/subfolders in folders starting with '_'
             ],
         },
 
@@ -104,13 +110,13 @@ module.exports = function() {
          */
 
         dest: {
-            root: destFolder,
-            html: destFolder,
-            css: destFolder + 'css/',
-            js: destFolder + 'js/',
-            img: destFolder + 'images/',
-            fonts: destFolder + 'fonts/',
-            docs: destFolder + 'docs/',
+            root: destFolder + '/',
+            html: destFolder + '/',
+            css: destFolder + '/css/',
+            js: destFolder + '/js/',
+            img: destFolder + '/images/',
+            fonts: destFolder + '/fonts/',
+            docs: destFolder + '/docs/',
             nodemodules: 'node_modules/',
         },
 
